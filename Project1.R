88a876bc75b89905c75b55e87e1961dc747f9dc5
RDITLMR9 <Mahbubur.Rashid@usace.army.mil>
2019-07-01 14:49:22 -0500

    Final commit

--- Project1.R
+++ Project1.R
@@ -22,7 +22,7 @@ total_df <- aggregate(activity$steps, by=list(activity$date), sum)
 colnames(total_df) <- c('Date', 'Total Steps')
 
 # Create a histogram from the aggregate data
-ggplot(data = total_df, aes(total_df$`Total Steps`)) + geom_histogram(col = 'red', fill = 'green', alpha = 0.2) + labs(title = 'Total Number of Steps Taken Per Day') + labs(x = 'Total Steps per Day', y = 'Number of Days') + ylim(c(0, 10))
+ggplot(data = total_df, aes(total_df$`Total Steps`)) + geom_histogram(binwidth = 1000, col = 'red', fill = 'green', alpha = 0.2) + labs(title = 'Total Number of Steps Taken Per Day') + labs(x = 'Total Steps per Day', y = 'Number of Days') + ylim(c(0, 10))
 mean_steps = mean(total_df$`Total Steps`, na.rm = TRUE)
 median_steps = median(total_df$`Total Steps`, na.rm = TRUE)
 
@@ -36,6 +36,7 @@ colnames(mean_df) <- c('Interval', 'Mean Steps')
 ggplot(mean_df, aes(x = mean_df$Interval, y = mean_df$`Mean Steps`)) + geom_line(col = 'blue', size = 1) + labs(title = 'Time Series of the Mean Number of Steps') + labs(x = 'Number of 5-Minute Intervals', y = 'Mean Number of Steps') + ylim(c(0, 250))
 maximum_average = mean_df[which.max(mean_df$`Mean Steps`),]
 maximum_average[,'Interval']
+maximum_average[,'Mean Steps']
 
 ##################################################################################################################
 ##
@@ -65,7 +66,7 @@ total_imputed_df <- aggregate(activity_orig$steps, by=list(activity_orig$date), 
 colnames(total_imputed_df) <- c('Date', 'Total Steps')
 
 # Create a histogram from the aggregate data
-ggplot(data = total_imputed_df, aes(total_imputed_df$`Total Steps`)) + geom_histogram(col = 'red', fill = 'green', alpha = 0.2) + labs(title = 'Total Number of Steps Taken Per Day') + labs(x = 'Total Steps per Day', y = 'Number of Days') + ylim(c(0, 10))
+ggplot(data = total_imputed_df, aes(total_imputed_df$`Total Steps`)) + geom_histogram(binwidth = 1000, col = 'red', fill = 'green', alpha = 0.2) + labs(title = 'Total Number of Steps Taken Per Day') + labs(x = 'Total Steps per Day', y = 'Number of Days') + ylim(c(0, 10))
 mean_imputed_steps = mean(total_imputed_df$`Total Steps`)
 median_imputed_steps = median(total_imputed_df$`Total Steps`)
 
